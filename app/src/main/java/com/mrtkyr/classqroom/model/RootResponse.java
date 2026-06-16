0d6ce064a903a6d95ac9b5e0ac1cf9a9c679cbb0
mertkiyar <mertkiyar69@gmail.com>
2026-06-16 17:52:11 +0300

    feat: add Response and User Model for backend responses

---
+++ app/src/main/java/com/mrtkyr/classqroom/model/RootResponse.java
@@ -0,0 +1,31 @@
+package com.mrtkyr.classqroom.model;
+
+public class RootResponse<T> {
+    private boolean result;
+    private String errorMessage;
+    private T data;
+
+    public boolean isResult() {
+        return result;
+    }
+
+    public void setResult(boolean result) {
+        this.result = result;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ app/src/main/java/com/mrtkyr/classqroom/model/UserModel.java
@@ -0,0 +1,58 @@
+package com.mrtkyr.classqroom.model;
+
+public class UserModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String userType;
+    private int departmentId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+}
