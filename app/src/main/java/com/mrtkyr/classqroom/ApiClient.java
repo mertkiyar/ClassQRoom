43f8c8ee894d97612f8b12d1a8ba18cc0883e8d4
mertkiyar <mertkiyar69@gmail.com>
2026-06-17 15:12:44 +0300

    refactor: update API_URL field

--- app/src/main/java/com/mrtkyr/classqroom/ApiClient.java
+++ app/src/main/java/com/mrtkyr/classqroom/ApiClient.java
@@ -8,7 +8,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    private static final String API_URL = "https://api.classqroom.mrtkyr.com";
+    private static final String API_URL = "https://api.classqroom.mrtkyr.com/";
     private static Retrofit retrofit;
     public static Retrofit getClient(Context context) {
         if (retrofit == null) {
