e6f0e411189da834144e0625ce6072cb3b81a30b
mertkiyar <mertkiyar69@gmail.com>
2026-08-19 18:03:51 +0300

    feat: add student api for profile fragment implementation

---
+++ app/src/main/java/com/mrtkyr/classqroom/api/StudentApi.java
@@ -0,0 +1,16 @@
+package com.mrtkyr.classqroom.api;
+
+import com.mrtkyr.classqroom.model.RootResponse;
+import com.mrtkyr.classqroom.model.StudentModel;
+
+import java.util.UUID;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface StudentApi {
+
+    @GET("/rest/api/student/get/{id}")
+    Call<RootResponse<StudentModel>> getStudentById(@Path("id") UUID studentId);
+}
